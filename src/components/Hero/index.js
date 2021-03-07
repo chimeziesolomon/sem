7e4399516974f1817d893cdbf39ab7a854f2e9b6
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2021-03-07 21:34:46 +0100

    resolved

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,62 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col } from 'reactstrap';
+//import { Container, Row, Col } from 'reactstrap';
 import bg1 from './../../assets/images/sem-spices.jpeg';
 
 function Hero() {
 	return (
-		<Container container>
-			<Row>
-				<Col className="justify-content-center">
-					<Col lg={6}>
-						<div className="hero-wrapper mb-4">
-							<h1
-								className="hero-title mb-4"
-								style={{
-									borderRadius: '70px',
-									width: '490px',
-									fontSize: '1.3rem',
-									textAlign: 'center',
-									letterSpacing: '3px'
-								}}
-							>
-								We are the leading foodcare solution provider <br /> {' '}
-								<span>Giving you healthier alternatives </span>
-								in Africa
-							</h1>
+		<div className="hero-container wrap">
+			<div className="hero-wrapper-top mb-4">
+				<h1 className="hero-title mb-4">
+					We are the leading foodcare solution provider <br /> {' '}
+					<span>Giving you healthier alternatives </span>
+					in Africa
+				</h1>
 
-							<div className="mt-4 rounded">
-								<Link
-									to="#"
-									className="btn  mt-2 mr-2"
-									style={{
-										borderRadius: '20px',
-										boxShadow: 'none',
-										width: '200px',
-										fontSize: 'small',
-										textAlign: 'center',
-										float: 'left',
-										marginLeft: '8rem'
-									}}
-								>
-									Getting Started
-								</Link>
-							</div>
-						</div>
-					</Col>
-				</Col>
-				<Col
-					className="hero-section"
-					id="home"
-					style={{
-						height: '140px',
-						borderRadius: '50%',
-						float: 'right',
-						background: `url(${bg1}) no-repeat`
-					}}
-				/>
-			</Row>
-		</Container>
+				<Link to="#" className="btn btn-rounded mt-4">
+					Getting Started
+				</Link>
+			</div>
+
+			<div
+				className="hero-section-top"
+				id="home"
+				style={{
+					background: `url(${bg1}) no-repeat center`,
+					backgroundSize: 'contain'
+				}}
+			/>
+		</div>
 	);
 }
 
